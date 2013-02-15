3f34205ff8e37899b0f5c67c009c3545aa754c42
Kenny Debrauwer <kenny.debrauwer@kunstmaan.be>
2013-02-15 16:36:19 +0100

    move some methods for Contentpages

--- Resources/skeleton/defaultsite/Form/AbstractContentPageAdminType.php
+++ Resources/skeleton/defaultsite/Form/AbstractContentPageAdminType.php
@@ -28,18 +28,6 @@ class AbstractContentPageAdminType extends AbstractType
     }
 
     /**
-     * Sets the default options for this type.
-     *
-     * @param OptionsResolverInterface $resolver The resolver for the options.
-     */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
-        $resolver->setDefaults(array(
-            'data_class' => '{{ namespace }}\Entity\ContentPage'
-        ));
-    }
-
-    /**
      * Returns the name of this type.
      *
      * @return string The name of this type
--- Resources/skeleton/defaultsite/Form/ContentPageAdminType.php
+++ Resources/skeleton/defaultsite/Form/ContentPageAdminType.php
@@ -12,4 +12,16 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 class ContentPageAdminType extends AbstractContentPageAdminType
 {
 
+    /**
+     * Sets the default options for this type.
+     *
+     * @param OptionsResolverInterface $resolver The resolver for the options.
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => '{{ namespace }}\Entity\ContentPage'
+        ));
+    }
+
 }

d1e84047f4b223e7a1242e1a27aa5f40d41ea7ed
Stéphane Bakelants <stephane.bakelants@kunstmaan.be>
2013-08-09 14:53:12 +0200

    removed login

--- Resources/SensioGeneratorBundle/skeleton/admintests/Features/Context/FeatureContext.php
+++ Resources/SensioGeneratorBundle/skeleton/admintests/Features/Context/FeatureContext.php
@@ -55,7 +55,7 @@ class FeatureContext extends AbstractContext
      */
     public function getPasswordForUsername($username)
     {
-        $logins = array('admin' => 'admin', 'test' => 'test', 'dummy' => 'dummy', 'wronguser' => 'wrongpassword');
+        $logins = array('admin' => 'admin', 'test' => 'test', 'dummy' => 'dummy');
 
         return $logins[$username];
     }
